000e22c610322b325982c45de64428fb7db37a83
sattysaini <stym.6996@gmail.com>
2019-05-30 21:16:37 +0530

    exponential python 1

---
+++ Exponential.py
@@ -0,0 +1,46 @@
+import numpy as np
+from kera.layers import Input, Dense
+from keras.models import Model 
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import os
+
+batch_size = 1024
+epochs = 10
+sessions = 5
+
+def exp():
+	inputs = Input((1,))
+	layer0 = Dense(512, activation = "relu", kernel_initializer = "random_uniform")(inputs)
+	layer1 = Dense(1024, activation = "relu", kernel_initializer = "random_uniform")(Layer0)
+	layer2 = Dense(1024, actication = "relu", kernel_initializer = "random_uniform")(Layer1)
+	outputs = Dense(1, activation = None)(Layer2)
+
+	model = Model(inputs = [inputs], outputs = [outputs])
+	adm = Adam(lr=0.001)
+	model.compile(optimizer = adm, loss = 'mean_absolute_error', metrics = ['mae'])
+	model.summary()
+	return model
+
+
+
+def main():
+	
+	model = exp()
+
+	for sess in range(sessions):
+		X = np.random.random_sample((batch_size,)) * 10 * np.pi - (5 * np.pi)
+		Y = np.exp(X)
+		for epoch in range(epochs):
+			model.fit(X, Y, valdation_split = 0.1)
+			y_predicted = model.predict()
+			fig, axs = plt.subplots(1, 2)
+			axs[0].scatter(X, Y, color = "green")
+			axs[1].scatter(X, Y, color = "red")
+			name = str(sess) + "_" + str(epoch) + ".png"
+			fig.savefig(os.path.join("exp^x_graphs", name))
+			plt.close()
+
+
+if __name__ == '__main__':
+	main()
