6ae786f82e67f5d54ea55f22a2c9f3df96f8a330
sattysaini <stym.6996@gmail.com>
2019-06-01 10:56:39 +0530

    error free code with output graphs

--- Exponential.py
+++ Exponential.py
@@ -1,5 +1,5 @@
 import numpy as np
-from kera.layers import Input, Dense
+from keras.layers import Input, Dense
 from keras.models import Model 
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
@@ -12,9 +12,9 @@ sessions = 5
 def exp():
 	inputs = Input((1,))
 	layer0 = Dense(512, activation = "relu", kernel_initializer = "random_uniform")(inputs)
-	layer1 = Dense(1024, activation = "relu", kernel_initializer = "random_uniform")(Layer0)
-	layer2 = Dense(1024, actication = "relu", kernel_initializer = "random_uniform")(Layer1)
-	outputs = Dense(1, activation = None)(Layer2)
+	layer1 = Dense(1024, activation = "relu", kernel_initializer = "random_uniform")(layer0)
+	layer2 = Dense(1024, activation = "relu", kernel_initializer = "random_uniform")(layer1)
+	outputs = Dense(1, activation = None)(layer2)
 
 	model = Model(inputs = [inputs], outputs = [outputs])
 	adm = Adam(lr=0.001)
@@ -32,13 +32,13 @@ def main():
 		X = np.random.random_sample((batch_size,)) * 10 * np.pi - (5 * np.pi)
 		Y = np.exp(X)
 		for epoch in range(epochs):
-			model.fit(X, Y, valdation_split = 0.1)
-			y_predicted = model.predict()
+			model.fit(X, Y, validation_split = 0.1)
+			y_predicted = model.predict(X)
 			fig, axs = plt.subplots(1, 2)
 			axs[0].scatter(X, Y, color = "green")
-			axs[1].scatter(X, Y, color = "red")
+			axs[1].scatter(X, y_predicted, color = "red")
 			name = str(sess) + "_" + str(epoch) + ".png"
-			fig.savefig(os.path.join("exp^x_graphs", name))
+			fig.savefig(os.path.join("F:\Git\ExponentialAssignment\exp^x_graphs", name))
 			plt.close()
 
 
